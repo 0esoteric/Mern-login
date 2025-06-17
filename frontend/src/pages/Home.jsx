5e9305bbfbbce0c0bdb969ee6a14a4a12ccc8d28
Harshit Sengar <125139050+0esoteric@users.noreply.github.com>
2025-06-17 23:14:58 +0530

    Update Home
    Added varcel link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
   }
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8080/products"
+      const url = "https://mern-login-api-eight.vercel.app/products"
       const headers = {
         headers: {
           'Authorization' : localStorage.getItem('token')
