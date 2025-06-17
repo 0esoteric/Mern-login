7b902ad63b7c08cf07e86657aee6facc8f19410b
Harshit Sengar <125139050+0esoteric@users.noreply.github.com>
2025-06-17 23:13:45 +0530

    Updated Signup.jsx
    Added varcel link

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -40,7 +40,7 @@ const Signup = () => {
     }
 
     try {
-      const url = "http://localhost:8080/auth/signup"
+      const url = "https://mern-login-api-eight.vercel.app/auth/signup"
       const response = await fetch(url, {
         method: "POST",
         headers: {
